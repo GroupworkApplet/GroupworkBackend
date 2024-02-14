2f29a486dc06624ecb9a416881666dd7913da5ab
Eachic <2331137574@qq.com>
2024-02-14 23:25:48 +0800

    增加了grouplist类

--- group/models.py
+++ group/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+from task.models import Task
+
+#小组的类
+class GroupList(models.Model):
+    group_name = models.CharField(max_length=10, verbose_name='小组名')
+    task = models.OneToOneField(Task, verbose_name='对应任务', on_delete=models.CASCADE)
+    group_members = models.ManyToManyField(User)
 
 # Create your models here.
