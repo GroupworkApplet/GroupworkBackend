64ce59435527330fae71ffa94832e2f1b1e0fabe
vertins <2493740752@qq.com>
2024-02-14 21:53:47 +0800

    new

--- common/models.py
+++ common/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+#通知
+class notification(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    message=models.TextField(max_length=100)
+    created_time=models.DateTimeField(auto_now_add=True)
+    is_read=models.BooleanField(default=False)
+    def __str__(self):
+        return self.message
