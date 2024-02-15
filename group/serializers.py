fe3b977caf2fced9b12b245a3753fa8c4465e692
Eachic <2331137574@qq.com>
2024-02-15 22:22:23 +0800

    增加了groupserializers

---
+++ group/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+
+from .models import *
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    task = serializers.CharField(source='task.title')
+    members = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+        read_only_fields = ('created_at',)
+
+
+class GroupMemberSerializer(serializers.ModelSerializer):
+    group = serializers.CharField(source='group.name')
+    user = serializers.CharField(source='user.name')
+
+    class Meta:
+        model = GroupMember
+        fields = '__all__'
+        read_only_fields = ('is_leader', 'joined_at',)
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    group = serializers.CharField(source='group.name')
+    sender = serializers.CharField(source='user.name')
+
+    class Meta:
+        model = Message
+        fields = '__all__'
+        read_only_fields = ('created_at',)
+
+
+class DocumentSerializer(serializers.ModelSerializer):
+    group = serializers.CharField(source='group.name')
+    sender = serializers.CharField(source='user.name')
+
+    class Meta:
+        model = Document
+        fields = '__all__'
+
+
+class ProgressSerializer(serializers.ModelSerializer):
+    group = serializers.CharField(source='group.name')
+
+    class Meta:
+        model = Progress
+        fields = '__all__'
