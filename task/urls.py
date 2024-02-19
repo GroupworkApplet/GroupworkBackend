cc6acb8edacfcc9300bc31357b25b3c7f1f5a7c7
hinhajk <3471435247@qq.com>
2024-02-19 15:42:09 +0800

    改了一下task的urls文件

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import TaskDetails, TaskProgressDetails, TaskAssignmentList, TaskAssignmentDetail
+from .views import TaskDetailsView, TaskProgressDetailsView, TaskAssignmentList, TaskAssignmentDetail
 
 urlpatterns = [
-    path("TaskDetails/", TaskDetails),
-    path("TaskProgressDetails/", TaskProgressDetails),
-    path("TaskAssignmentList/", TaskAssignmentList),
-    path("TaskAssignmentDetail/", TaskAssignmentDetail),
+    path('task/<int:pk>/', TaskDetailsView.as_view(), name='task-details'),
+    path('task-progress/<int:pk>/', TaskProgressDetailsView.as_view(), name='task-progress-details'),
+    path('task-assignments/', TaskAssignmentList.as_view(), name='task-assignment-list'),
+    path('task-assignments/<int:pk>/', TaskAssignmentDetail.as_view(), name='task-assignment-detail'),
 ]
