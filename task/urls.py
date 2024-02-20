fc05141cd29ca9cd03b85025dcf540c822b8e733
Charmian <lizecan0605@gmail.com>
2024-02-20 15:00:43 +0800

    修改

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from .views import TaskDetailsView, TaskProgressDetailsView, TaskAssignmentList, TaskAssignmentDetail
+from .views import (
+    CreateTaskView,
+    TaskDetailsView,
+    TaskProgressDetailsView,
+    TaskAssignmentList,
+    TaskAssignmentDetail
+)
 
 urlpatterns = [
-    path('task/<int:pk>/', TaskDetailsView.as_view(), name='task-details'),
+    # 任务创建
+    path('tasks/', CreateTaskView.as_view(), name='create-task'),
+    # 任务详情（获取、更新、删除）
+    path('tasks/<int:pk>/', TaskDetailsView.as_view(), name='task-details'),
+    # 任务进度详情（获取、更新）
     path('task-progress/<int:pk>/', TaskProgressDetailsView.as_view(), name='task-progress-details'),
-    path('task-assignments/', TaskAssignmentList.as_view(), name='task-assignment-list'),
+    # 任务分配列表（获取、创建）
+    path('task-assignments/', TaskAssignmentList.as_view(), name='task-assignments-list'),
+    # 任务分配详情（获取、更新、删除）
     path('task-assignments/<int:pk>/', TaskAssignmentDetail.as_view(), name='task-assignment-detail'),
 ]
+
