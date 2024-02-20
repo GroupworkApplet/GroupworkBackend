7b22a4ba6df559fd27f2898a026b2d2dbf8fbee9
Charmian <lizecan0605@gmail.com>
2024-02-21 01:58:42 +0800

    task修改

--- settlement/views.py
+++ settlement/views.py
@@ -2,6 +2,9 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+from task.models import Task
+from user.models import UserCredit
 from .models import TaskSettlement, MemberContribution
 from .serializers import TaskSettlementSerializer, MemberContributionSerializer
 
@@ -9,32 +12,76 @@ from .serializers import TaskSettlementSerializer, MemberContributionSerializer
 class TaskSettlementView(APIView):
     def get(self, request, task_id):
         try:
-            settlement = TaskSettlement.objects.get(task_id=task_id)
+            task = get_object_or_404(Task, pk=task_id)
+            settlement = get_object_or_404(TaskSettlement, task=task)
             serializer = TaskSettlementSerializer(settlement)
-            return Response(serializer.data)
+            response_data = {
+                'task_id': task_id,
+                'is_completed': serializer.data['is_completed'],
+                'completion_time': serializer.data['completion_time']
+            }
+            return Response(response_data)
         except TaskSettlement.DoesNotExist:
             return Response({'错误': '任务不存在'}, status=status.HTTP_404_NOT_FOUND)
 
-
-class MemberContributionView(APIView):
-    def get(self, request, settlement_id):
-        contributions = MemberContribution.objects.filter(settlement_id=settlement_id)
-        serializer = MemberContributionSerializer(contributions, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = MemberContributionSerializer(data=request.data)
+    def post(self, request, task_id):
+        task = get_object_or_404(Task, pk=task_id)
+        serializer = TaskSettlementSerializer(data=request.data, context={'task': task})
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            settlement = serializer.save()
+            task_id = settlement.task_id
+            response_data = {
+                'task_id': task_id,
+                'is_completed': serializer.data['is_completed'],
+                'completion_time': serializer.data['completion_time']
+            }
+            return Response(response_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class UpdateCreditView(APIView):
-    def post(self, request, contribution_id):
-        try:
-            contribution = MemberContribution.objects.get(id=contribution_id)
+class MemberContributionView(APIView):
+    def get(self, request, task_id):
+        task = get_object_or_404(Task, pk=task_id)
+        settlement = get_object_or_404(TaskSettlement, task=task)
+        contributions = MemberContribution.objects.filter(settlement=settlement)
+        serializer = MemberContributionSerializer(contributions, many=True)
+        response_data = {
+            'task_id': task_id,
+            'contributions': serializer.data
+        }
+        return Response(response_data)
+
+    def post(self, request, task_id):
+        task = get_object_or_404(Task, pk=task_id)
+        settlement = get_object_or_404(TaskSettlement, task=task)
+        serializer = MemberContributionSerializer(data=request.data, context={'settlement': settlement})
+        if serializer.is_valid():
+            contribution = serializer.save()
+            task_id = contribution.settlement.task_id
+            response_data = {
+                'task_id': task_id,
+                'member': serializer.data['member'],
+                'contribution': serializer.data['contribution']
+            }
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, task_id):
+        task = get_object_or_404(Task, pk=task_id)
+        settlement = get_object_or_404(TaskSettlement, task=task)
+        contributions = MemberContribution.objects.filter(settlement=settlement)
+
+        updated_contributions = []
+        for contribution in contributions:
+            # 更新每个成员的信用分
             contribution.update_credit()
-            return Response({'提示': '信用分更新成功'})
-        except MemberContribution.DoesNotExist:
-            return Response({'错误': '成员贡献不存在'}, status=status.HTTP_404_NOT_FOUND)
+            updated_contributions.append({
+                'member': contribution.member.username,
+                'contribution': contribution.contribution,
+                'updated_credit': UserCredit.objects.get(user=contribution.member).credit
+            })
+
+        return Response({
+            'task_id': task_id,
+            'updated_contributions': updated_contributions
+        }, status=status.HTTP_200_OK)
