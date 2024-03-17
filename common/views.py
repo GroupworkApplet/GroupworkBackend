8a104bdbd884207645d9382f7bf5d6570ea3b270
vertins <2493740752@qq.com>
2024-03-17 15:54:15 +0800

    要在总文件下设置一个upload文件夹

--- common/views.py
+++ common/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from .models import Notification
 from .serializers import NotificationSerializer
 
